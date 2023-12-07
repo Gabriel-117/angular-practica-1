cc21d652cd3f22990895c5e73d56d434c1ed42b9
Gabriel-117 <gabi495@hotmail.com>
2023-12-06 19:55:56 -0600

    Practica 1

---
+++ src/app/mi-componente/mi-componente.component.ts
@@ -0,0 +1,82 @@
+import { Component, OnInit } from '@angular/core';
+import { Tarea } from './models/tarea.model';
+import { MenuItem } from './models/menu-item.model';
+
+@Component({
+  selector: 'app-mi-componente',
+  templateUrl: './mi-componente.component.html',
+  styleUrls: ['./mi-componente.component.css']
+})
+export class MiComponenteComponent implements OnInit {
+  // public menuItems: {item:string, active:boolean}[] = [];
+  // Opciones para el menú
+  menuItems: MenuItem[] = [];
+  // public tareas: {titulo:string, descripcion:string, status:string}[] = [];
+  // Lista de tareas guardadas
+  tareas: Tarea[] = [];
+  // Objeto para agregar tareas
+  newTarea: Tarea = {
+    titulo: 'l,askdjaslkdhnaskjd', 
+    descripcion: '',
+    status: 'Pendiente'
+  };
+
+  constructor() {
+    // let item1: MenuItem = {item: '', active: false};
+    // this.menuItems.push(item1);
+    this.menuItems.push( {item: 'Nueva tarea', active: false} );
+    this.menuItems.push( {item: 'Mis tareas', active: true} );
+  }
+
+  ngOnInit(): void {
+    const tareasJson = localStorage.getItem("tareas");
+    if (tareasJson != null) {
+      this.tareas = JSON.parse(tareasJson);
+    }
+  }
+
+  // public setMenuItemAsActive(item: {item:string, active:boolean}): void {
+  //   for (let menuItem of this.menuItems){
+  //     if (menuItem == item){
+  //       menuItem.active = true;
+  //     }
+  //     else {
+  //       menuItem.active = false;
+  //     }
+  //   }
+  // }
+  public setMenuItemAsActive(item: number): void {
+    for (let x = 0; x<this.menuItems.length; x++){
+      if (x == item){
+        this.menuItems[x].active = true;
+        continue;
+      }
+      
+      this.menuItems[x].active = false;
+    }
+  }
+
+  public agregarTarea( titulo: string, descripcion: string ): void {
+    this.newTarea.titulo = titulo;
+    this.newTarea.descripcion = descripcion;
+
+    this.tareas.push(this.newTarea);
+    this.newTarea = {
+      titulo: '',
+      descripcion: '',
+      status: 'Pendiente'
+    }
+    this.setMenuItemAsActive(1);
+
+    this.almacenarDatos();
+  }
+
+  public cambiarStatus(index:number, status: string): void {
+    this.tareas[index].status = status;
+    this.almacenarDatos();
+  }
+
+  private almacenarDatos(): void {
+    localStorage.setItem("tareas", JSON.stringify(this.tareas));
+  }
+}
